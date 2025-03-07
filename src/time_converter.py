6a6e1e29fde2251dba3e51a9ce69b0fe870c50b8
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2025-03-08 02:22:21 +0500

    Update time_converter.py

--- src/time_converter.py
+++ src/time_converter.py
@@ -30,6 +30,18 @@ class TimeConverter:
             return self.hours_to_days(value)
         elif from_unit == "Days" and to_unit == "Hours":
             return self.days_to_hours(value)
+        elif from_unit == "Seconds" and to_unit == "Hours":
+            return self.seconds_to_minutes(self.minutes_to_hours(value))
+        elif from_unit == "Hours" and to_unit == "Seconds":
+            return self.hours_to_minutes(self.minutes_to_seconds(value))
+        elif from_unit == "Seconds" and to_unit == "Days":
+            return self.seconds_to_minutes(self.minutes_to_hours(self.hours_to_days(value)))
+        elif from_unit == "Days" and to_unit == "Seconds":
+            return self.days_to_hours(self.hours_to_minutes(self.minutes_to_seconds(value)))
+        elif from_unit == "Minutes" and to_unit == "Days":
+            return self.minutes_to_hours(self.hours_to_days(value))
+        elif from_unit == "Days" and to_unit == "Minutes":
+            return self.days_to_hours(self.hours_to_minutes(value))
         else:
             return value  # If the units are the same or conversion is not defined
 
